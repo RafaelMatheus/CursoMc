4cb3b033a8402741a94bd629541dc7d0ff343fbc
Rafael Matheus <rafaelmatheusdecastro@hotmail.com.br>
2018-05-12 18:07:37 -0300

    Populando o banco quando o codigo for carregado e a aplicacao inicar

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,12 +1,33 @@
 package com.nelioalves.cursomc;
 
+import org.assertj.core.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+	@Autowired
+	private CategoriaRepository repo;
+	
+	
+	public static void main(String[] args)  {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+	
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Saúde");
+		
+		repo.saveAll(java.util.Arrays.asList(cat1,cat2, cat3));
+		
+		
 	}
 }
